ffbe446ad58308a9e447dc83e226d7264d4e2311
Tangue Muweh <35699075+tmuweh@users.noreply.github.com>
2018-06-01 00:06:20 +0200

    Create ruler.js
    vert and hori ruler

---
+++ ruler.js
@@ -0,0 +1,13 @@
+fill(17, 0, 255);
+
+// a handy dandy ruler across the top
+var x = 0;
+while(x < 400){
+    text(x, x, 10);
+    x += 50;
+}
+var y = 0;
+while(y < 400){
+     text (y, 10, y);
+     y += 50;
+ }
